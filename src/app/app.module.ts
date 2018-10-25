67c1773f3d62db5428c1bcc718e330bf84b0ef72
James Inglis <jamesekinglis@gmail.com>
2018-10-24 19:48:11 -0600

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { BannerComponent } from './banner/banner.component';
+import { InfoCardComponent } from './info-card/info-card.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    BannerComponent,
+    InfoCardComponent,
+    ContactUsComponent
   ],
   imports: [
     BrowserModule
