cb06dddf2d3a33d579ff20b591f3b8d45d073ca0
James Inglis <jamesekinglis@gmail.com>
2018-12-01 23:32:30 -0700

    slideshow is working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,19 +6,21 @@ import { DOCUMENT } from '@angular/common';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
 
   constructor(@Inject(DOCUMENT) document) { }
-
-  @HostListener('window:scroll', ['$event'])
-  onWindowScroll(e) {
-    if (window.pageYOffset > 0) {
-      const element = document.getElementById('navbar');
-      element.classList.add('sticky');
-    } else {
-      const element = document.getElementById('navbar');
-      element.classList.remove('sticky');
-    }
+  ngOnInit() {
   }
+
+@HostListener('window:scroll', ['$event'])
+onWindowScroll(e) {
+  if (window.pageYOffset > 0) {
+    const element = document.getElementById('navbar');
+    element.classList.add('sticky');
+  } else {
+    const element = document.getElementById('navbar');
+    element.classList.remove('sticky');
+  }
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,21 +8,10 @@ import { InfoCardComponent } from './info-card/info-card.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule, Routes } from '@angular/router';
+import {SlideshowModule} from 'ng-simple-slideshow';
 
 const appRoutes: Routes = [
   { path: 'info-card', component: InfoCardComponent }
-  // { path: 'crisis-center', component: CrisisListComponent },
-  // { path: 'hero/:id',      component: HeroDetailComponent },
-  // {
-  //   path: 'heroes',
-  //   component: HeroListComponent,
-  //   data: { title: 'Heroes List' }
-  // },
-  // { path: '',
-  //   redirectTo: '/heroes',
-  //   pathMatch: 'full'
-  // },
-  // { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
@@ -41,10 +30,11 @@ const appRoutes: Routes = [
       { enableTracing: true , // <-- debugging purposes only
       scrollPositionRestoration: 'enabled',
       anchorScrolling: 'enabled',
-      scrollOffset: [0, 30], // [x, y],
+      scrollOffset: [0, 20], // [x, y],
       onSameUrlNavigation: 'reload'
       }
-    )
+    ),
+    SlideshowModule
   ],
   providers: [],
   bootstrap: [AppComponent]
