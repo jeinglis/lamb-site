ac565b7dd4f6260927ca9b27ead97f0c0e38859b
James Inglis <jamesekinglis@gmail.com>
2018-12-22 15:11:11 -0400

    built

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { BannerComponent } from './banner/banner.component';
 import { InfoCardComponent } from './info-card/info-card.component';
-import { ContactUsComponent } from './contact-us/contact-us.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule, Routes } from '@angular/router';
 import { SlideshowModule } from 'ng-simple-slideshow';
@@ -25,7 +24,6 @@ const appRoutes: Routes = [
     NavigationComponent,
     BannerComponent,
     InfoCardComponent,
-    ContactUsComponent,
   ],
   imports: [
     BrowserModule,
